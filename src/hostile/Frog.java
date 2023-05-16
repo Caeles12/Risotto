d30104c6f4a0f8b4a37fdb4fa75c135bea18ea03
flaflouu <flavienleborgne25@gmail.com>
2023-05-16 11:22:27 +0200

    Hostile and Frog

---
+++ src/hostile/Frog.java
@@ -0,0 +1,52 @@
+package hostile;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import entity.Hostile;
+import main.GamePanel;
+
+public class Frog extends Hostile {
+
+	double m_dir =2*3.14;
+	
+	public Frog(GamePanel a_gp){
+		super( a_gp);
+	}
+
+	@Override
+	protected void setDefaultValues() {
+		m_x = 200;
+		m_y = 100;
+		m_life = 100;
+		m_damage = 10;
+		m_speed = 2;
+		
+	}
+
+	@Override
+	public void getHostileImage() {
+		//gestion des expections 
+		try {
+			m_idleImage = ImageIO.read(getClass().getResource("/Player/superhero.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	protected void move() {
+		
+		if (m_timer > 120) {
+			m_x += m_speed;
+		}
+		
+		if (m_timer > 180) {
+			m_dir = r.nextFloat()*2*3.14;
+			m_timer = 0;
+		}
+
+		
+	}
+}
