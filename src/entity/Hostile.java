144a3c81d530744040ece3b8b2389030b3db6c85
flaflouu <flavienleborgne25@gmail.com>
2023-05-16 23:42:00 +0200

    hostile collision version final and clean de hostile et grenouille

--- src/entity/Hostile.java
+++ src/entity/Hostile.java
@@ -9,6 +9,9 @@ import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.Renderer;
+import utils.Collider;
+import utils.Rectangle;
+import utils.Vector2D;
 
 abstract public class Hostile extends Entity{
 
@@ -19,6 +22,8 @@ abstract public class Hostile extends Entity{
 	protected int m_timerAnimation;
 	protected Random r = new Random();
 	protected int ptr_list_image = 0;
+	protected Collider m_collider;
+	protected int[] m_dir;
 	
 	/**
 	 * Constructeur de Hostile
@@ -28,6 +33,7 @@ abstract public class Hostile extends Entity{
 		this.m_gp = a_gp;
 		this.setDefaultValues();
 		this.getHostileImage();
+		this.m_collider = new Collider(new Rectangle(m_pos, new Vector2D(a_gp.TILE_SIZE/4, a_gp.TILE_SIZE/4),a_gp.TILE_SIZE/2, a_gp.TILE_SIZE/2), a_gp);
 	}
 	
 	/**
@@ -39,6 +45,7 @@ abstract public class Hostile extends Entity{
 	 * R�cup�ration de l'image du monstre
 	 */
 	abstract public void getHostileImage();
+	abstract protected void animationRate();
 	
 	/**
 	 * Mise � jour des donn�es du monstre
@@ -47,22 +54,43 @@ abstract public class Hostile extends Entity{
 		m_timer ++;
 		m_timerAnimation ++;
 		move();
+		animationRate();
 		
 	}
 	
 	/**
 	 * Movement du monstre
 	 */
-	protected abstract void move();
+	protected void move()
+	{
+		float norme = 1;
+		this.m_collider.m_shape.setOrigin(m_pos);
+		
+		if(m_dir[0] != 0 || m_dir[1] != 0) {
+			norme = (float) Math.sqrt(m_dir[0] * m_dir[0] + m_dir[1] * m_dir[1]);
+		}
+		
+		float vx = (m_dir[0] / norme);
+		float vy = (m_dir[1] / norme);
+		
+		this.m_pos.x += vx*m_speed;
+		if(this.m_collider.collidingTileMap(this.m_gp.m_tileM)) {
+			this.m_pos.x -= vx*m_speed;
+		}
+		this.m_pos.y += vy*m_speed;
+		if(this.m_collider.collidingTileMap(this.m_gp.m_tileM)) {
+			this.m_pos.y -= vy*m_speed;
+		}
+	}
+	
+	protected void lifeBar() {
+		
+	}
 	
 	/**
 	 * Affichage du l'image du monstre dans la fen�tre du jeu
 	 * @param a_g2 Graphics2D 
 	 */
-	
-	protected void lifeBar() {
-		
-	}
 	
 	public void draw(Renderer a_g2) {
 		// r�cup�re l'image du joueur
--- src/hostile/Frog.java
+++ src/hostile/Frog.java
@@ -6,14 +6,16 @@ import javax.imageio.ImageIO;
 
 import entity.Hostile;
 import entity.SpeechBubble;
+
 import main.GamePanel;
 import main.Renderer;
+
 import utils.Vector2D;
+
 import tile.Tile;
 
 public class Frog extends Hostile {
 
-	int m_dir =0;
 	
 	public Frog(GamePanel a_gp,int x,int y){
 		super( a_gp);
@@ -28,6 +30,7 @@ public class Frog extends Hostile {
 		m_timer = 0;
 		ptr_list_image = 0;
 		m_timerAnimation = 0;
+		m_dir = new int[2];
 		
 	}
 
@@ -44,76 +47,27 @@ public class Frog extends Hostile {
 
 	protected void move() {
 		
-		if(m_timerAnimation >= 15) {
-			ptr_list_image++;
-			m_timerAnimation = 0;
-		}
 		
 		if (m_timer > 120) {
 			
-			
-			
-			
-			switch(m_dir) {
-			case 1 : 
-				if (!collideWallNext(this.m_pos.x+m_speed, this.m_pos.y)) this.m_pos.x += m_speed;
-				break;
-			case 2 :
-				if (!collideWallNext(this.m_pos.x-m_speed, this.m_pos.y)) this.m_pos.x -= m_speed;
-				break;
-			case 3 :
-				if (!collideWallNext(this.m_pos.x, this.m_pos.y+m_speed)) this.m_pos.y += m_speed;
-				break;
-			case 4 :
-				if (!collideWallNext(this.m_pos.x, this.m_pos.y-m_speed)) this.m_pos.y -= m_speed;
-				break;
-			default :
-				break;
-			}
+			super.move();
 			
 		}
 		
 		if (m_timer > 150) {
 			new SpeechBubble(m_gp, "Croa",(int) this.m_pos.x,(int) this.m_pos.y, 4, 15, 2);
-			m_dir = r.nextInt(5);
+			m_dir[0] = r.nextInt(3)-1;
+			m_dir[1] = r.nextInt(3)-1;
+			System.out.println("i'm here !");
 			m_timer = 0;
 		}
-
-		//System.out.println(distWall());
 	}
 	
-	/*
-	 * Fonction qui verifie si le monstre finit dans le mur apres le prochain movement
-	 * @param x coordonnees en x du monstre
-	 * @param y coordonnees en x du monstre
-	 */
-	boolean collideWallNext(float x, float y) {
-
-		
-		int posX = (int)x/m_gp.TILE_SIZE; 
-		int posY = (int)y/m_gp.TILE_SIZE;
-		Tile[] m_tile = m_gp.m_tileM.getTile();
-		
-		
-		if(m_tile[m_gp.m_tileM.getMapTile(posX, posY)].m_collision == true) //on verifie la tile de gauche
-		{
-			return true;
+	protected void animationRate() {
+		if(m_timerAnimation >= 15) {
+			ptr_list_image++;
+			m_timerAnimation = 0;
 		}
-		else if(m_tile[m_gp.m_tileM.getMapTile(posX+1, posY)].m_collision == true) //on verifie la tile de droite
-		{
-			return true;
-		}
-		else if(m_tile[m_gp.m_tileM.getMapTile(posX, posY)].m_collision == true) //on verifie la tile du haut
-		{
-			return true;
-		}
-		else if(m_tile[m_gp.m_tileM.getMapTile(posX, posY+1)].m_collision == true) //on verifie la tile du bas
-		{
-			return true;
-		}
-			
-		return false;
 	}
-
 
 }
