bc1cb5adac306c39855a972dd6f733f0ec0e0dbb
Pandonline <geffroy250@gmail.com>
2023-05-16 11:04:43 +0200

    conflict solve animated

--- src/entity/Camera.java
+++ src/entity/Camera.java
@@ -1,12 +1,14 @@
 package entity;
 
 import main.GamePanel;
+import main.Renderer;
+
 
 public class Camera extends Entity{
 	
-	private float m_scale;
-	
+	private float m_scale;	
 	public Camera(GamePanel a_gp, int pos_x, int pos_y, float scale) {
+
 		this.m_x = pos_x;
 		this.m_y = pos_y;
 		this.m_scale = scale;
@@ -24,5 +26,11 @@ public class Camera extends Entity{
 	
 	public float getScale() {
 		return m_scale;
+	}
+
+	@Override
+	public void draw(Renderer a_g2) {
+		// TODO Auto-generated method stub
+		
 	}
 }
